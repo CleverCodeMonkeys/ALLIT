aa9b0f2288ee2da603bbe079abf363d0edbdb4a6
user2 <user2@KH_H>
2018-08-02 21:16:28 +0900

    commit

--- CCMS/src/main/java/com/kh/ccms/correction/model/dao/CorrectionDaoImple.java
+++ CCMS/src/main/java/com/kh/ccms/correction/model/dao/CorrectionDaoImple.java
@@ -68,4 +68,10 @@ public class CorrectionDaoImple implements CorrectionDao {
 		return sqlSession.update("correction.updateCorrection", correction);
 	}
 
+	@Override
+	public List<Map<String, String>> selectResume(String id) {
+		// TODO Auto-generated method stub
+		return sqlSession.selectList("correction.selectResume", id);
+	}
+
 }
